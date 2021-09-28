9d4fcce511c07da6793b1e76e4b298fd6955692e
Brian Reynolds <brianreynolds65@gmail.com>
2021-09-28 18:27:34 +1000

    testing 1.5

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+const { promisify } = require('util');
+const { resolve } = require('path');
+const fs = require('fs');
+const readdir = promisify(fs.readdir);
+const stat = promisify(fs.stat);
 const core = require('@actions/core');
 const github = require('@actions/github');
-var fs = require('fs');
-const { walk } = require('./file');
+// var fs = require('fs');
+
+async function getFiles(dir) {
+  const subdirs = await readdir(dir);
+  const files = await Promise.all(subdirs.map(async (subdir) => {
+    const res = resolve(dir, subdir);
+    return (await stat(res)).isDirectory() ? getFiles(res) : res;
+  }));
+  return files.reduce((a, f) => a.concat(f), []);
+}
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -13,11 +26,11 @@ try {
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
   console.log('PWD:', process.cwd());
-  walk(process.cwd(), (err, results) => {
-    if (err) throw err;
-    console.log(results);
-  })
   
+  getFiles(process.cwd())
+  .then(files => console.log(files))
+  .catch(e => { console.error(e); core.setFailed(e)});
+
   // var files = fs.readdirSync(process.cwd());
   // files.forEach((file) => {
   //   console.log(file)
